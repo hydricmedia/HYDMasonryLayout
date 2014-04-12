d69e6cfead012d7586f99079b54f3e1a56c91166
Simon Hall <simonhall01@gmail.com>
2014-04-12 11:59:21 +1000

    Initial Commit

---
+++ HYDMasonryLayout/HYDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYDAppDelegate.h
+//  HYDMasonryLayout
+//
+//  Created by Simon Hall on 12/04/2014.
+//  Copyright (c) 2014 Simon Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HYDMasonryLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HYDMasonryLayout
+//
+//  Created by Simon Hall on 12/04/2014.
+//  Copyright (c) 2014 Simon Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HYDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYDAppDelegate class]));
+    }
+}
